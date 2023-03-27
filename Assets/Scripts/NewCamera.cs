277ac1a69660ec56f23bdb4afd005fd3b592c8cb
Saul Bravo <sauluriel1402@gmail.com>
2023-03-26 17:47:07 -0700

    win and die scenes, can take picture when having camera pieces

--- Assets/Scripts/NewCamera.cs
+++ Assets/Scripts/NewCamera.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class NewCamera : MonoBehaviour
 {
@@ -10,11 +11,14 @@ public class NewCamera : MonoBehaviour
     public GameObject cameraBroken;
     public TextMeshProUGUI needPiecesMessage;
     public TextMeshProUGUI putCameraMessage;
+    public TextMeshProUGUI takePictureMessage;
     private bool isColliding = false;
+    private bool cameraPlaced = false;
 
     void Start(){
         needPiecesMessage.gameObject.SetActive(false);
         putCameraMessage.gameObject.SetActive(false);
+        takePictureMessage.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -22,9 +26,19 @@ public class NewCamera : MonoBehaviour
     {
         if(isColliding && Gamepad.current != null && Gamepad.current.buttonWest.wasPressedThisFrame){
             Inventory inventory = Resources.Load<Inventory>("Inventory");
+            Vector3 cameraPosition = new Vector3(-6.59695625f,1.722f,-16.8862514f);
+            Quaternion rotation = Quaternion.Euler(0f, -80f, 0f);
+
+            if(cameraPlaced){
+                SceneManager.LoadScene("WinScene");
+            }
            
             if(inventory.inventory.Contains("CameraPiece2") && inventory.inventory.Contains("CameraPiece1")){
                 Destroy(cameraBroken);
+                GameObject newObject = Instantiate(cameraPrefab,cameraPosition,rotation);
+                putCameraMessage.gameObject.SetActive(false);
+                takePictureMessage.gameObject.SetActive(true);
+                cameraPlaced=true;
             }
             else {
                 putCameraMessage.gameObject.SetActive(false);
@@ -35,7 +49,12 @@ public class NewCamera : MonoBehaviour
 
     void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Player")) {
-            putCameraMessage.gameObject.SetActive(true);
+            if(cameraPlaced){
+                takePictureMessage.gameObject.SetActive(true);
+            }
+            else{
+                putCameraMessage.gameObject.SetActive(true);
+            }
             isColliding = true;
         }
     }
@@ -44,8 +63,9 @@ public class NewCamera : MonoBehaviour
         if (other.CompareTag("Player")) {
             putCameraMessage.gameObject.SetActive(false);
             needPiecesMessage.gameObject.SetActive(false);
+            takePictureMessage.gameObject.SetActive(false);
             isColliding = false;
         }
     }
 }
-// Vector3(-11.2880001,6.375,-3.94499993)
+// 
